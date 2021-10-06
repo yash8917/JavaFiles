36574c969662745d2f9710f8863327cdf6fcf60d
Yash Gupta <yash8917gupta@gmail.com>
2021-10-06 22:50:13 +0530

    Initial commit

---
+++ src/TestAbstractBank.java
@@ -0,0 +1,17 @@
+abstract class Bank{
+    abstract int getRateOfIntrest();
+}
+class SBI extends Bank{
+    int getRateOfIntrest(){return 7;}
+}
+class PNB extends Bank{
+    int getRateOfIntrest(){return 8;}
+}
+public class TestAbstractBank {
+    public static void main(String[] args) {
+        Bank obj=new SBI();
+        System.out.println("The Rate of Intrest of SBI Bank is: " +obj.getRateOfIntrest()+" %");
+        Bank obj1=new PNB();
+        System.out.println("The Rate of Intrest of PNB Bank is: "+obj1.getRateOfIntrest()+" %");
+    }
+}
