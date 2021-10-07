6eb78c8b0ebf87a0bbb2509c6e3dd7340642608f
Yash Gupta <yash8917gupta@gmail.com>
2021-10-07 21:12:59 +0530

    Create the Interface class

---
+++ src/JavaInterfaceExa.java
@@ -0,0 +1,16 @@
+interface  printable{
+    void print();
+}
+
+public class JavaInterfaceExa implements printable {
+    public void print() {
+        System.out.println("HELLO");
+    }
+
+    public static void main(String[] args) {
+        JavaInterfaceExa obj=new JavaInterfaceExa();
+        obj.print();
+    }
+
+}
+
