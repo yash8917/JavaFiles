3f8c992d60910979bd5559a20232fc5dcc603a21
Yash Gupta <yash8917gupta@gmail.com>
2021-10-12 00:47:15 +0530

    Pakage Elaborate

--- src/p1/Stu.java
+++ src/p1/Stu.java
@@ -7,6 +7,7 @@ public class Stu extends Person{
     public Stu(int marks, int rollno,int age,String name) {
         super(age, name);      //child class se imidiate parent class ke constructer ko invoke karega
         // super statement be the first line
+
         this.marks = marks;
         this.rollno = rollno;
 //        this.age=age;         to solve the redundancy of code we use super statement
