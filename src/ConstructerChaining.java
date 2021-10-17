82e9b2dbc529cb4c17068b0440e50a9838993690
Yash Gupta <yash8917gupta@gmail.com>
2021-10-17 16:16:03 +0530

    INITIAL COMMIT

---
+++ src/ConstructerChaining.java
@@ -0,0 +1,20 @@
+class Stu{
+    int rollno;
+    String name;
+
+    public Stu() {
+        System.out.println("This is non-parameterize constructer");
+    }
+    public Stu(int rollno,String name){
+        this();
+        System.out.println("THis is parametrize constructer");
+
+    }
+}
+public class ConstructerChaining {
+    public static void main(String[] args) {Stu obj=new Stu(69,"Yash Gupta.");
+        //Stu obj1=new Stu();
+        Stu obj2=new Stu(69,"Yash Gupta.");
+
+    }
+}
