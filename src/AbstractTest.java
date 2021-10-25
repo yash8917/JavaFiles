e6a943e7d19326c2c1cfc4471973371a40b052cc
Yash Gupta <yash8917gupta@gmail.com>
2021-10-26 01:57:29 +0530

    INITIAL COMMIT

---
+++ src/AbstractTest.java
@@ -0,0 +1,24 @@
+abstract class Cellphone{
+abstract void sim();
+}
+class Redmi extends Cellphone{
+    @Override
+    void sim() {
+        System.out.println("Redmi has a dual sim.");
+    }
+}
+class Vivo extends Cellphone{
+    @Override
+    void sim() {
+        System.out.println("Vivo has Quad sim.");
+    }
+
+}
+public class AbstractTest {
+    public static void main(String[] args) {
+        Cellphone obj=new Redmi();
+        obj.sim();
+        Cellphone obj2=new Vivo();
+        obj2.sim();
+    }
+}
