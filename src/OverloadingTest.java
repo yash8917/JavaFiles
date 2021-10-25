a645da9d6bc7d431b343f5c11a25555d9c9ea152
Yash Gupta <yash8917gupta@gmail.com>
2021-10-25 23:31:37 +0530

    INITIAL COMMIT

---
+++ src/OverloadingTest.java
@@ -0,0 +1,14 @@
+public class OverloadingTest {
+    public void m1(){
+        System.out.println("Non parameterize method ");
+    }
+    public void m1(int a,int b){
+        System.out.println("sum is:"+(a+b));
+    }
+
+    public static void main(String[] args) {
+        OverloadingTest obj=new OverloadingTest();
+        obj.m1();
+        obj.m1(23,5);
+    }
+}
