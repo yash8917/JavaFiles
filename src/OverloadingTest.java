e52ac96dc5897d43114d2cbe36e65ffedaac85f1
Yash Gupta <yash8917gupta@gmail.com>
2021-10-25 23:54:19 +0530

    INITIAL COMMIT

--- src/OverloadingTest.java
+++ src/OverloadingTest.java
@@ -5,7 +5,6 @@ public class OverloadingTest {
     public void m1(int a,int b){
         System.out.println("sum is:"+(a+b));
     }
-
     public static void main(String[] args) {
         OverloadingTest obj=new OverloadingTest();
         obj.m1();
