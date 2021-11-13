50d23c2fe42abff89bcdea0e2901ba04a62d20c2
Yash Gupta <yash8917gupta@gmail.com>
2021-11-13 22:37:40 +0530

    INITIAL COMMIT

---
+++ src/PatientMain.java
@@ -0,0 +1,20 @@
+public class PatientMain{
+    String name;
+    double h;
+    double w;
+
+    public PatientMain(String name, double h, double w) {
+        this.name = name;
+        this.h = h;
+        this.w = w;
+    }
+
+    public double computeBmi(){
+        return w/(h*h);
+    }
+
+    public static void main(String[] args) {
+        PatientMain obj=new PatientMain("Yash",14,33);
+        System.out.println( obj.computeBmi());
+    }
+}
