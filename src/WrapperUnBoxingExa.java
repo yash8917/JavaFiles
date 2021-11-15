f97b02f9a7e12728bf22dda61d021236bc719610
Yash Gupta <yash8917gupta@gmail.com>
2021-11-15 21:28:48 +0530

    INITIAL COMMIT

---
+++ src/WrapperUnBoxingExa.java
@@ -0,0 +1,9 @@
+public class WrapperUnBoxingExa {
+    public static void main(String[] args) {
+
+        Integer a=new Integer(4);
+//        convert the Object in to primitive
+        int i=a;
+        System.out.println(a+" "+i);
+    }
+}
