72e26756f9e02fe752bbe1c10408788b604b9aba
Yash Gupta <yash8917gupta@gmail.com>
2021-11-16 19:56:42 +0530

    INITIAL COMMIT

---
+++ src/WrapperAssignment_1.java
@@ -0,0 +1,10 @@
+public class WrapperAssignment_1 {
+    public static void main(String[] args) {
+        System.out.println("Integer-:\n"+"Max Value\n"+Integer.MAX_VALUE+"\nMin Value+\n"+Integer.MIN_VALUE);
+        System.out.println("Float Value\n"+"Max Value:\n"+Float.MAX_VALUE+"\nMin Value:\n"+Float.MIN_VALUE);
+        System.out.println("Double Value\n"+"Max Value:\n"+Double.MAX_VALUE+"\nMin Value:\n"+Double.MIN_VALUE);
+        System.out.println("Long Value\n"+"Max Value:\n"+Long.MAX_VALUE+"\n Min Value:\n"+Long.MIN_VALUE);
+        System.out.println("Character value\n"+"Max value:\n"+Character.MAX_VALUE+"\n Min Value:\n"+Character.MIN_VALUE);
+        System.out.println("Short Value:\n"+"Max value:\n"+Short.MAX_VALUE+"\nMin Value:\n"+Short.MIN_VALUE);
+    }
+}
