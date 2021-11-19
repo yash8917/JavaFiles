84ff246fee92f7356d3c1b3c3a088fbf2fc7aa70
Yash Gupta <yash8917gupta@gmail.com>
2021-11-19 23:39:38 +0530

    INITIAL COMMIT

---
+++ src/ArrayInputUser.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ArrayInputUser {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of the Array:-");
+        int n= sc.nextInt();
+        int arr[]=new int[n];
+        for (int i = 0; i <n; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array element's are:-");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
