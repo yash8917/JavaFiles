16535831c91d512fad1a09659d356d25ba3bafff
Yash Gupta <yash8917gupta@gmail.com>
2021-11-19 23:46:28 +0530

    INITIAL COMMIT

--- src/ArrayInputUser.java
+++ src/ArrayInputUser.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class ArrayInputUser {
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the size of the Array:-");
