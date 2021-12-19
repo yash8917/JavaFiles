564b3cef5b91c527291700afd01e58f0458210b9
Yash Gupta <yash8917gupta@gmail.com>
2021-12-19 09:34:37 +0530

    INITIAL COMMIT

--- src/TryCatchFinally.java
+++ src/TryCatchFinally.java
@@ -2,6 +2,7 @@ public class TryCatchFinally {
     public static void main(String[] args) {
         int a = 20, b = 0;
         try {
+
             System.out.println("Value of a: " + a);
             System.out.println("Value of b: " + b);
             int div = a / b;
