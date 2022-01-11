85f09a2572222bc3c658a7eea02b16ae5285c07b
Yash Gupta <yash8917gupta@gmail.com>
2022-01-11 23:10:30 +0530

    INITIAL COMMIT

--- src/WrapperAssignment_2.java
+++ src/WrapperAssignment_2.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 
 public class WrapperAssignment_2 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Give the Number:\n");
 
 //        Using Command line argument and store the wrapper Class
-        Integer a= sc.nextInt(){
-            
+        Integer a = sc.nextInt() ;{
+
 //        Print the Values
-        System.out.println("Binary Equivalant: "+Integer.toBinaryString(a));
-        System.out.println("\nHexa Equivalant: "+Integer.toHexString(a));
-        System.out.println("\nOctal Equivalant: "+Integer.toOctalString(a));
+            System.out.println("Binary Equivalant: " + Integer.toBinaryString(a));
+            System.out.println("\nHexa Equivalant: " + Integer.toHexString(a));
+            System.out.println("\nOctal Equivalant: " + Integer.toOctalString(a));
+
+        }
     }
 }
